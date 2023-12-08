4043bed64cf1a014b812e37c223cc6195f200051
dudcoco <213954@jnu.ac.kr>
2023-12-08 23:07:49 +0900

    여행 코스 추천 코드 추가 및 모듈화

--- Sources/course_recommendation.py
+++ Sources/course_recommendation.py
@@ -7,34 +7,73 @@ def read_file(filename):
     return df
 
 # 여행 코스를 추천해주는 함수
-def CourseRecommend(region, files):
+def CourseRecommend(region):
+    # 구역 번호에 맞는 구역 장소 파일 이름을 저장함
+    if region == 1:
+        files =  ["jeju1_tour_region.csv",
+                  "jeju1_cafe_souvenir.csv",
+                  "jeju1_restaurant.csv"]
+    elif region == 2:
+        files =  ["jeju2_tour_region.csv",
+                  "jeju2_cafe_souvenir.csv",
+                  "jeju2_restaurant.csv"]
+    elif region == 3:
+        files =  ["jeju3_tour_region.csv",
+                  "jeju3_cafe_souvenir.csv",
+                  "jeju3_restaurant.csv"]
+    elif region == 4:
+        files =  ["jeju4_tour_region.csv",
+                  "jeju4_cafe_souvenir.csv",
+                  "jeju4_restaurant.csv"]
+    
     courses = []
     
     for idx, file in enumerate(files):
         df = read_file(file)
         if 'stores' in df.columns:
-            place_list = df['stores'].tolist()
+            place_list = df['stores'].tolist() # 장소명 리스트
+            url_list = df['place_url'].tolist()  # 장소 url 가져와서 리스트에 저장
             
             # 파일순으로 하나씩 선택
-            selected_place = random.choice(place_list)
-            courses.append(selected_place)
+            selected_index = random.choice(range(len(place_list)))
+            selected_place = place_list[selected_index]
+            selected_url = url_list[selected_index] if 'place_url' in df.columns else "url 없음"
+            
+            # 장소와 url을 저장
+            courses.append((selected_place, selected_url))
     
-    print("======== 여행 코스 추천 ========")
+    print("================ 여행 코스 추천 ================")
     print(f"선택하신 {region}구역의 여행 코스를 랜덤으로 추천해 드립니다.")
-    for i, place in enumerate(courses, start=1):
-        if i == 1:
-            print(f"{i}.관광지 - {place}")
-        elif i == 2:
-            print(f"{i}.카페 및 기념품 - {place}")
-        elif i == 3:
-            print(f"{i}.음식점 - {place}")
-
-
-
-if __name__ == "__main__":
-    file_paths = ["jeju1_tour_region.csv",
-              "jeju1_cafe_souvenir.csv",
-              "jeju1_restaurant.csv"]
     
-    CourseRecommend(1, file_paths)
+    # 징소를 랜덤으로 추천해주고 장소의 url도 함께 출력함
+    for i, (place, url) in enumerate(courses, start=1):
+        if i == 1:
+            print(f"{i}.관광지 - {place} ({url})")
+        elif i == 2:
+            print(f"{i}.카페 및 기념품 - {place} ({url})")
+        elif i == 3:
+            print(f"{i}.음식점 - {place} ({url})")
+            print()
+    ContinueRecommend(region)
 
+# 코스를 계속 추천받을지 입력받는 함수
+def ContinueRecommend(region_num):
+    while True:
+        diff_course = input("다른 코스를 추천받으시겠어요? (Y / N(종료)) ")
+        print()
+        # y 입력 시 새로운 코스 추천
+        if diff_course.lower() == "y":
+            CourseRecommend(region_num)
+        # n 입력 시 프로그램을 종료함
+        elif diff_course.lower() == "n":
+            print("여행 코스 추천 프로그램을 종료합니다.")
+            break
+        # 다른 문자 입력 시 문자를 재입력받음
+        else:
+            print("올바른 문자를 입력해주세요.")
+            continue
+        break
+
+# 코드 테스트
+if __name__ == "__main__":
+    CourseRecommend(1) # 1구역
--- Sources/main.py
+++ Sources/main.py
@@ -1,46 +1,51 @@
-import jeju_region as region
-
+import jeju_region as rg
+import course_recommendation as cr
 # 사용자가 권역을 선택하고 출발지를 입력함
 try:
+
     while True:
         print("====== 제주도 여행 코스 추천 프로그램 ======")
         print("1. 제주시  2. 제주서부  3. 서귀포시  4. 제주동부")
         print("5. 권역의 세부 지역 정보 설명")
-        user_input = int(input("여행을 원하는 권역의 번호를 선택하세요. -> "))
+        region_num = int(input("여행을 원하는 권역의 번호를 선택하세요. -> "))
 
         # 선택한 번호에 맞게 코스 추천 메시지 출력
-        if user_input == 1:
-            print("제주시의 여행 코스를 추천해드릴게요.")
-            # 여기에 코스 추천 함수 삽입
+        if region_num == 1:
+            print("제주시의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num)
             break
-        elif user_input == 2:
-            print("제주서부의 여행 코스를 추천해드릴게요.")
+        elif region_num == 2:
+            print("제주서부의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num)
             break
-        elif user_input == 3:
-            print("서귀포시의 여행 코스를 추천해드릴게요.")
+        elif region_num == 3:
+            print("서귀포시의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num)
             break
-        elif user_input == 4:
-            print("제주동부의 여행 코스를 추천해드릴게요.")
+        elif region_num == 4:
+            print("제주동부의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num)
             break
         
         # 세부 지역 정보 표시하기(각 권역 별 동 정보)
-        elif user_input == 5:
+        elif region_num == 5:
             region_info = int(input("어느 권역의 세부 지역 정보가 필요하신가요?(숫자 입력) "))
+            print()
             if region_info == 1:
                 print("1. 제주시 권역의 세부 지역 정보는 다음과 같습니다.")
-                print(region.jeju1)
+                print(rg.jeju1, "\n")
                 continue
             elif region_info == 2:
                 print("2. 제주서부 권역의 세부 지역 정보는 다음과 같습니다.")
-                print(region.jeju2)
+                print(rg.jeju2, "\n")
                 continue
             elif region_info == 3:
                 print("3. 서귀포시 권역의 세부 지역 정보는 다음과 같습니다.")
-                print(region.jeju3)
+                print(rg.jeju3, "\n")
                 continue
             elif region_info == 4:
                 print("4. 제주동부 권역의 세부 지역 정보는 다음과 같습니다.")
-                print(region.jeju4)
+                print(rg.jeju4, "\n")
                 continue
         # 잘못된 권역 번호 입력 시
         else:
