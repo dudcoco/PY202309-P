88868d9d56762aec18587004948cd78c7322dfca
dudcoco <213954@jnu.ac.kr>
2023-12-23 03:28:33 +0900

    코스 추천 코드 테스트 및 코드 다듬기

--- Sources/course_recommendation.py
+++ Sources/course_recommendation.py
@@ -7,8 +7,8 @@ def read_file(filename):
     df = pd.read_csv(filename, encoding='cp949')
     return df
 
-# 위도 경도를 이용하여 두 지점 간의 거리를 계산하는 함수
-def calculate_distance(coord1, coord2):
+# 위도, 경도를 이용하여 두 지점 간의 거리를 계산하는 함수
+def calculate_distance(coord1, coord2): 
     return geodesic(coord1, coord2).km
 
 # 여행 코스를 추천해주는 함수
@@ -31,13 +31,15 @@ def CourseRecommend(region):
                  "jeju4_cafe_souvenir.csv",
                  "jeju4_restaurant.csv"]
 
-
     courses = {"1": {"data": []},
                "2": {"data": []},
                "3": {"data": []}}
 
+    # 파일에서 장소를 불러와 선택
     for idx, file in enumerate(files):
-        df = read_file(file)
+        df = read_file(file) # 파일 읽기
+        
+        # csv 파일 안의 위도, 경도 값을 가져옴
         if 'x' in df.columns and 'y' in df.columns:
             coord_list = df.apply(lambda row: (row['y'], row['x']), axis=1).tolist()
 
@@ -48,10 +50,10 @@ def CourseRecommend(region):
             # 관광지 선택
             place = df.loc[selected_index, 'stores']
             url = df.loc[selected_index, 'place_url'] if 'place_url' in df.columns else "url 없음"
-            courses["1"]["data"].append((place, url))
-            print()
+            courses["1"]["data"].append((place, url)) # 장소 추가
 
-            # 카페 및 기념품 선택 
+            # 카페 및 기념품 선택
+            # 선택된 관광지에서 7km 반경 이내의 장소를 선택
             cafe_souvenir_candidates = [i for i, coord in enumerate(coord_list) if
                                         calculate_distance(selected_coord, coord) <= 7 and df.loc[i, 'stores'] not in
                                         [p[0] for p in courses["1"]["data"]]]
@@ -59,9 +61,10 @@ def CourseRecommend(region):
             if selected_cafe_souvenir is not None:
                 place = df.loc[selected_cafe_souvenir, 'stores']
                 url = df.loc[selected_cafe_souvenir, 'place_url'] if 'place_url' in df.columns else "url 없음"
-                courses["2"]["data"].append((place, url))
+                courses["2"]["data"].append((place, url)) # 장소 추가
 
-                # 음식점 선택 
+                # 음식점 선택
+                # 선택된 카페 및 기념품에서 7km 반경 이내의 장소를 선택
                 restaurant_candidates = [i for i, coord in enumerate(coord_list) if
                                          calculate_distance(coord_list[selected_cafe_souvenir], coord) <= 7 and df.loc[
                                              i, 'stores'] not in [p[0] for p in courses["2"]["data"]]]
@@ -69,30 +72,42 @@ def CourseRecommend(region):
                 if selected_restaurant is not None:
                     place = df.loc[selected_restaurant, 'stores']
                     url = df.loc[selected_restaurant, 'place_url'] if 'place_url' in df.columns else "url 없음"
-                    courses["3"]["data"].append((place, url))
-                
-            
+                    courses["3"]["data"].append((place, url)) # 장소 추가
+
+    # 코스 추천 메시지 출력
     print("================ 여행 코스 추천 ================")
     print(f"선택하신 {region}구역의 하루 여행 코스를 랜덤으로 추천해 드립니다.")
     print("( 1.관광지 > 2.카페 및 기념품 > 3.음식점 > 4.관광지 > 5.카페 및 기념품 > 6.음식점 순서로 추천합니다.)\n")
     print("                    시작")
     print("                     ↓")
 
-    # 
-    for category in ["1", "2"]:
-        for i in range(3): 
-            place, url = courses[category]["data"][i]
-            print(f"- {place} ({url})")
-            print("                     ↓")
-    print("             하루 여행 코스 끝")
-    ContinueRecommend(region)
+    # 선택한 구역별로 출발 기준지를 출력 (출발기준지 선택기준 - 숙박시설이 상대적으로 많은 장소로 선택)
+    if region == 1:
+        print("- 출발 기준지: 제주국제공항 (https://place.map.kakao.com/10808261)")
+    elif region == 2:
+        print("- 출발 기준지: 고내포구 (https://place.map.kakao.com/8175875)")
+    elif region == 3:
+        print("- 출발 기준지: 서귀포버스터미널 (https://place.map.kakao.com/7938158)")
+    elif region == 4:
+        print("- 출발 기준지: 고성교차로 (https://place.map.kakao.com/15140307)")
+    print("                     ↓")
     
+    # 관광지 > 카페 및 기념품 > 음식점 > 관광지 > 카페 및 기념품 > 음식점 순서로 출력
+    for category in ["2", "3"]:
+        for i in range(3):
+            place, url = courses[category]["data"][i]
+            print(f"- {place} ({url})") # 장소와 url을 함께 출력
+            print("                     ↓")
+
+    print("             하루 여행 코스 끝")
+    ContinueRecommend(region) # 코스를 계속 추천받을지 사용자에게 입력받음
+
 # 코스를 계속 추천받을지 입력받는 함수
 def ContinueRecommend(region_num):
     while True:
         diff_course = input("\n다른 코스를 추천받으시겠어요? (Y / N(종료)) ")
         print()
-        # y 입력 시 새로운 코스 추천
+        # y 입력 시 새로운 코스 랜덤 추천
         if diff_course.lower() == "y":
             CourseRecommend(region_num)
         # n 입력 시 프로그램을 종료함
@@ -108,3 +123,4 @@ def ContinueRecommend(region_num):
 # 코드 테스트
 if __name__ == "__main__":
     CourseRecommend(1)  # 1구역
+    
--- Sources/main.py
+++ Sources/main.py
@@ -1,30 +1,36 @@
-import jeju_region as rg
-import course_recommendation as cr
+try:
+    import jeju_region as rg
+    import course_recommendation as cr
+
+except ModuleNotFoundError:
+    print("실행에 필요한 모듈이 설치되어 있지 않습니다. pandas, random, geopy 모듈을 설치한 후 실행해주세요.")
+    exit(0) # 모듈 오류 메시지 출력 후 프로그램 즉시 종료
+
 # 사용자가 권역을 선택하고 출발지를 입력함
 try:
 
     while True:
-        print("====== 제주도 여행 코스 추천 프로그램 ======")
+        print("========== 제주도 여행 코스 추천 프로그램 ==========")
         print("1. 제주시  2. 제주서부  3. 서귀포시  4. 제주동부")
         print("5. 권역의 세부 지역 정보 설명")
         region_num = int(input("여행을 원하는 권역의 번호를 선택하세요. -> "))
 
         # 선택한 번호에 맞게 코스 추천 메시지 출력
         if region_num == 1:
-            print("제주시의 여행 코스를 추천해드릴게요.\n")
-            cr.CourseRecommend(region_num)
+            print("\n제주시의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num) # 코스 추천 함수 호출
             break
         elif region_num == 2:
-            print("제주서부의 여행 코스를 추천해드릴게요.\n")
-            cr.CourseRecommend(region_num)
+            print("\n제주서부의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num) # 코스 추천 함수 호출
             break
         elif region_num == 3:
-            print("서귀포시의 여행 코스를 추천해드릴게요.\n")
-            cr.CourseRecommend(region_num)
+            print("\n서귀포시의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num) # 코스 추천 함수 호출
             break
         elif region_num == 4:
-            print("제주동부의 여행 코스를 추천해드릴게요.\n")
-            cr.CourseRecommend(region_num)
+            print("\n제주동부의 여행 코스를 추천해드릴게요.\n")
+            cr.CourseRecommend(region_num) # 코스 추천 함수 호출
             break
         
         # 세부 지역 정보 표시하기(각 권역 별 동 정보)
@@ -47,11 +53,18 @@ try:
                 print("4. 제주동부 권역의 세부 지역 정보는 다음과 같습니다.")
                 print(rg.jeju4, "\n")
                 continue
+            else:
+                print("올바른 권역 번호를 입력해주세요.\n")
         # 잘못된 권역 번호 입력 시
         else:
             print("올바른 권역 번호를 입력해주세요.")
             continue
 
 # 오류 발생 시 예외처리
+# 파일을 찾을 수 없는 경우 예외처리
+except FileNotFoundError:
+    print("실행에 필요한 장소 csv 파일이 존재하지 않습니다. 파일을 올바른 경로에 위치시킨 후 실행해주세요.")
+
+# 그외 모든 오류 예외처리
 except:
-    print("오류가 발생했습니다.")
+    print("오류가 발생했습니다. 모듈 설치 여부나 파일 존재 여부 등을 확인해주세요.")
